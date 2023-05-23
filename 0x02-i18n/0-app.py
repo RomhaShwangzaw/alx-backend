7f8cad501892be79709a2e9431a696127a315449
Romha Shwangzaw <romhas@gmail.com>
2023-05-23 15:31:13 +0300

    Fixed index filename in render_template

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def index() -> str:
     """The home/index page"""
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
